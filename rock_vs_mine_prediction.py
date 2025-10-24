e50d0889fd46c3ac8c27674ad7ce39cf282ef144
Manav Bansal <bansalmanav39@gmail.com>
2025-10-23 21:20:20 -0700

    Updated the code for more resilience

--- rock_vs_mine_prediction.py
+++ rock_vs_mine_prediction.py
@@ -6,26 +6,42 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
+import pickle
+import warnings
+warnings.filterwarnings('ignore')
 
 """Data Collection and Data Processing"""
 
-# loading the dataset to a pandas DataFrame
-sonar_data = pd.read_csv('sonar_data.csv', header=None)  # No header in the CSV file
+try:
+    # loading the dataset to a pandas DataFrame
+    sonar_data = pd.read_csv('sonar_data.csv', header=None)  # No header in the CSV file
+    print("Dataset loaded successfully.")
+    print(f"Dataset Shape: {sonar_data.shape}")
+except FileNotFoundError:
+    print("Error: The dataset file was not found.")
+    exit()
+
 
 # Checking the first 5 rows of the dataset
-# print(sonar_data.head())
+print("\nFirst 5 rows of the dataset:")
+print(sonar_data.head())
 
-# number of rows and columns in the dataset
-#print(sonar_data.shape)  
+# The number of missing values in the dataset
+print(f"\nMissing values: {sonar_data.isnull().sum().sum()}") 
 
 # statistical measures about the data
-#print(sonar_data.describe())
+print("\nStatistical summary of the dataset:")
+print(sonar_data.describe())
 
 # checking the distribution of Target Variable
+print("\nDistribution of Target Variable:")
 #print(sonar_data[60].value_counts())    # Mines -> 111  Rocks -> 97
+print(f"M (Mine): {(sonar_data[60] == 'M').sum()}")
+print(f"R (Rock): {(sonar_data[60] == 'R').sum()} \n")
 
 # Grouping the data based on Target Variable
-#print(sonar_data.groupby(60).mean())
+print("Mean values of features grouped by Target Variable:")
+print(sonar_data.groupby(60).mean())
 
 # Seperating data and labels
 X = sonar_data.drop(columns = 60, axis = 1)
@@ -35,8 +51,12 @@ Y = sonar_data[60]
 # print(Y)
 
 """Splitting the data into Training data and Test Data"""
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, stratify=Y, random_state=1)
-print(X.shape, X_train.shape, X_test.shape)
+X_train, X_test, Y_train, Y_test = train_test_split(
+    X, Y, test_size=0.1, stratify=Y, random_state=1
+    )
+print(f"\nOriginal dataset: {X.shape}")
+print(f"Training set: {X_train.shape}")
+print(f"Test set: {X_test.shape}")
 
 """Model Training - Logistic Regression Model"""
 model = LogisticRegression()
