07e13b1d6193d826aa00651529240737ee6a0be5
Manav Bansal <bansalmanav39@gmail.com>
2025-10-23 19:36:57 -0700

    Added required dependencies

---
+++ rock_vs_mine_prediction.py
@@ -0,0 +1,8 @@
+""" Rock vs Mine Prediction Model """
+
+"""Importing the Dependencies"""
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
